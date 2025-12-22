d56eb6ec181da4fd72646c45e8a6f2b74e71e0d4
Brian Lassiter <51713030+belassiter@users.noreply.github.com>
2025-12-21 18:47:27 -0800

    firebase deployment

--- packages/games/turn-seven/src/main.tsx
+++ packages/games/turn-seven/src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { TurnSevenGame } from './index';
 import './styles.css';
 import './mobile.css';
+import { auth } from '@turn-seven/engine';
+import { signInAnonymously } from 'firebase/auth';
+
+signInAnonymously(auth)
+  .then((creds) => {
+    console.log('Signed in anonymously', creds.user.uid);
+  })
+  .catch((error) => {
+    console.error('Anonymous sign-in failed', error);
+  });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
